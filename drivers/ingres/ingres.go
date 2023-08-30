0e49f20e04bb6f94b83e9909c8a959e153c8c42a
Ildus <ildus.kurbangaliev@actian.com>
2023-08-30 06:42:47 +0000

    more info

--- drivers/ingres/ingres.go
+++ drivers/ingres/ingres.go
@@ -10,7 +10,9 @@ import (
 	"github.com/ildus/usql/drivers"
 	md "github.com/ildus/usql/drivers/metadata"
 
+	"context"
 	"io"
+        "fmt"
 )
 
 func init() {
@@ -18,6 +20,29 @@ func init() {
 		NewMetadataReader: NewIngresReader,
 		NewMetadataWriter: func(db drivers.DB, w io.Writer, opts ...md.ReaderOption) md.Writer {
 			return NewIngresWriter(NewIngresReader(db, opts...))(db, w)
+		},
+		Version: func(ctx context.Context, db drivers.DB) (string, error) {
+			var out string
+			err := db.QueryRowContext(ctx, `SELECT DBMSINFO('_VERSION');`).Scan(&out)
+			if err != nil || out == "" {
+				out = "<unknown>"
+			}
+			return out, nil
+		},
+		User: func(ctx context.Context, db drivers.DB) (string, error) {
+			var out string
+			err := db.QueryRowContext(ctx, `SELECT DBMSINFO('username');`).Scan(&out)
+			if err != nil || out == "" {
+				out = "<unknown>"
+			}
+			return out, nil
+		},
+		ChangePassword: func(db drivers.DB, user, new, old string) (error) {
+			_, err := db.Exec(fmt.Sprintf(`ALTER USER %s WITH PASSWORD= '%s' `, user, new))
+                        if err != nil {
+                            return err
+                        }
+                        return nil
 		},
 	})
 }
